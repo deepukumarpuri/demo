aad1c7a53e3593a3360e9fc39e2c9877b17e67c2
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-07-26 13:27:30 +0530

    Update shazam.py

--- DaisyX/modules/shazam.py
+++ DaisyX/modules/shazam.py
@@ -46,7 +46,7 @@ async def shazamm(client, message):
     messageo = f"""<b>Song Shazamed.</b>
 <b>Song Name : </b>{title}
 <b>Song By : </b>{by}
-<u><b>Identified Using @DaisyXBot - Join our support @DaisySupport_Official</b></u>
+<u><b>Identified Using @DKBOTZ - Join our support @DK_BOTZ</b></u>
 <i>Powered by @FridayOT</i>
 """
     await client.send_photo(message.chat.id, image, messageo, parse_mode="HTML")
